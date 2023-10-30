1f1f70f503140552bf562eac39428221e313ae6f
Prud'Homme Gateau Sebastien <sebastien.prudhomme-gateau@etu.umontpellier.fr>
2023-10-30 17:48:28 +0100

    recuperation pour vacances

---
+++ src/main/java/org/example/Config/Config.java
@@ -0,0 +1,9 @@
+package org.example.Config;
+
+public class Config {
+
+    public static String projectSourcePath =  "/home/e20170009949/IdeaProjects/TP1_P2";
+
+    public static String jrePath = System.getProperty("java.home");
+
+}
